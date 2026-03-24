1b31005134c635b9aebc1bc08996e7ec9054f255
nityaaajain <jenith1975@gmail.com>
2026-03-24 11:32:09 +0530

    Lec01

---
+++ Week01/Lec01/c1.java
@@ -0,0 +1,30 @@
+
+public class c1 {
+    public static void main(String[] args) {
+        int[] a=new int[5];
+        a[0]=2;
+        a[1]=5;
+        a[2]=3;
+        a[3]=6;
+        a[4]=1;
+        int min=a[0],max=a[0];
+        int sum=0;
+        
+        for(int i=0;i<a.length;i++){
+            if(a[i]>max){
+                max=a[i];
+            }
+            if(a[i]<min){
+                min=a[i];
+
+            }
+            sum=sum+a[i];
+
+        }
+        System.err.println(min);
+        System.err.println(max);
+        float avg= (float)sum/a.length;
+        System.err.println(avg);
+    }
+    
+}
---
+++ Week01/Lec01/t2.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class t2 {
+     public static void main(String[] args){
+       ArrayList<Integer>list=new ArrayList<>(); 
+       list.add(6);
+       list.add(7);
+       list.add(8);
+       list.add(9);
+       list.add(10);
+
+    }
+
+}
