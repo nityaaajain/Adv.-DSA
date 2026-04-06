96b0b4b1749136fa3740032bb9a9b8154de67b1e
nityaaajain <jenith1975@gmail.com>
2026-04-06 13:54:22 +0530

    Day4

---
+++ JAVA/Lec04/matrix.java
@@ -0,0 +1,18 @@
+
+public class matrix{
+    public static void main(String[] args) {
+        int a[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int sum=0;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(i==j){
+                    sum = sum + a[i][j];
+                }
+            }
+
+        }
+        System.out.println(sum);
+    
+}
+
+}
